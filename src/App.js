cb7d1819d08a9970fef9fcb2f4a216c9974e28e9
yiyujin <yujin94@gmail.com>
2025-01-04 15:09:10 -0500

    a little more organized component

--- src/App.js
+++ src/App.js
@@ -9,19 +9,18 @@ function App() {
 
         <img/>
 
-        <div>
-          <h2>Name</h2>
+        <div style={{ paddingTop: "24px", paddingBottom: "24px" }}>
+          <h2 style={{ marginBottom: "16px" }}>Name</h2>
 
-          <div style = { { display : "flex" } }>
-            <img className = "type"/>
+          <div style={{ display: "flex", height: "56px" }}>
+            <img className="type" style={{ height: "100%" }} />
 
-            <div>
-              <h3>Type</h3>
-              <p>Tags</p>
+            <div style={{ height: "100%", marginLeft: "16px", display: "flex", flexDirection: "column" }}>
+              <h3 style={{ margin: "0" }}>Type</h3>
+              <p style={{ marginTop: "auto" }}>Tags</p>
             </div>
           </div>
         </div>
-
       </div>
 
     </div>
