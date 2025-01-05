e9b4930b613ede14fff5c6386ecf3c9ebe2fba71
yiyujin <yujin94@gmail.com>
2025-01-04 21:05:17 -0500

    accept both changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className = "App">
-
-      <div className = "Component">
-
-        <img/>
-
-        <div>
-          <h2>Name</h2>
-
-          <div style = { { display : "flex" } }>
-            <img className = "type"/>
-
-            <div>
-              <h3>Type</h3>
-              <p>Tags</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-=======
 import './App.css';
 import Component from './Component';
 
@@ -37,13 +9,8 @@ function App() {
         <Component name = "꼬부기" type = "Water" tags = {["grass", "poison"]}/>
         <Component name = "피카츄" type = "Thunder" tags = {["electric"]}/>
       </div>
->>>>>>> temp
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> temp
