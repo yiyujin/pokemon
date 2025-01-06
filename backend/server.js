009897e8fd3d8016d5e2eab2731d5fc700a8e006
yiyujin <yujin94@gmail.com>
2025-01-05 23:38:27 -0500

    fetch from react - run into cors error

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,13 @@ import { TOKEN, DATABASE_ID } from "./config.js";
 import express from 'express';
 const app = express();
 
-const PORT = 8080;
+const PORT = 8000;
 
 app.listen(PORT, function(){
     console.log(`listening to ${PORT}`);
-
-    console.log(TOKEN);
 })
 
+//retrieve a database
 export async function retrieveADatabase(){
   const notion = new Client({ auth: TOKEN });
 
@@ -26,3 +25,23 @@ app.get("/api/retrieve-a-database", async (req, res) => {
     const results = await retrieveADatabase();
     res.json(results);
 });
+
+//Query a database
+export async function queryADatabase() {
+    const notion = new Client({ auth: TOKEN });
+  
+    const response = await notion.databases.query({
+      database_id: DATABASE_ID,
+    });
+  
+    return response.results;
+  }
+  
+  app.post("/api/query-a-database", async (req, res) => {
+    try {
+      const results = await queryADatabase();
+      res.json(results);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,35 @@
 import './App.css';
 import Component from './Component';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  //GET DATA
+  const fetchData = async () => {        
+    try {
+      const response = await fetch("http://localhost:8000/api/query-a-database", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className = "App" style = { { display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: "100%", paddingTop : "80px", paddingBottom : "80px", margin : "0 auto"} }>
       <div className = "grid">
