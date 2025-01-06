74aae791f45a84a6defe016f5d1a3e9e5d4d4dbb
yiyujin <yujin94@gmail.com>
2025-01-06 00:43:27 -0500

    fetch data and change component css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,6 @@ function App() {
       }
 
       const result = await response.json();
-
-      console.log(result);
       setData(result);
     } catch (error) {
       console.error("Error fetching data:", error.message);
@@ -35,9 +33,10 @@ function App() {
   return (
     <div className = "App" style = { { display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: "100%", paddingTop : "80px", paddingBottom : "80px", margin : "0 auto"} }>
       <div className = "grid">
-        <Component name = "파이리" type = "Fire" tags = {["fire", "lizard"]}/>
-        <Component name = "꼬부기" type = "Water" tags = {["grass", "poison"]}/>
-        <Component name = "피카츄" type = "Thunder" tags = {["electric"]}/>
+
+        { data.map( ( item, index ) => (
+            <Component key = { index } item = { item }/>
+        ))}
       </div>
     </div>
   );
--- frontend/src/Component.js
+++ frontend/src/Component.js
@@ -1,21 +1,43 @@
-export default function Component( { name, type, tags } ){
-    return(
-        <div className = "Component">
+export default function Component( { item } ){
+  // name, type, tags, url
+  
+  const name = item.properties.Name.title[0].plain_text;
+  const type = item.properties.Type.rich_text[0].plain_text;
+  const tags = item.properties.Tags.multi_select;
+  const url = item.properties.URL.rich_text[0].plain_text;
+  const thumbnail = item.icon.file.url;
 
-        <img/>
+  let bg;
 
-        <div style={{ paddingTop: "24px", paddingBottom: "24px" }}>
-          <h2 style={{ marginBottom: "16px" }}>{ name }</h2>
+  if( type === "Fire"){
+    bg = "rgba(245, 100, 112, 0.1)";
+  } else if ( type === "Seed"){
+    bg =  "rgba(99, 213, 105, 0.1)";
+  } else if ( type === "Thunder" ){
+    bg = "rgba(255, 152, 1, 0.1)";
+  }
 
-          <div style={{ display: "flex", height: "56px" }}>
-            <img className="type" style={{ height: "100%" }} />
+  return(
+      <div className = "Component">
 
-            <div style={{ height: "100%", marginLeft: "16px", display: "flex", flexDirection: "column" }}>
-              <h3 style={{ margin: "0" }}>{ type }</h3>
-              <p style={{ marginTop: "auto" }}>{ tags.join(", ") }</p>
-            </div>
+      <div className = "imgContainer">
+        <img src = { url }/>
+      </div>
+
+      <div style = { { paddingTop: "12px", paddingBottom: "24px" } }>
+        <h2 style = { { marginBottom: "40px" } }>{ name }</h2>
+
+        <div style = { { display: "flex" } }>
+          <div className = "typeContainer" style = { { backgroundColor : bg } }>
+            <img className = "type" src = { thumbnail }/>
+          </div>
+
+          <div style = { { height: "100%", marginLeft: "16px", display: "flex", flexDirection: "column" } }>
+            <h3 style = { { margin: "0" } }>{ type }</h3>
+            <p style = { { marginTop: "auto", paddingTop : "6px" } }>{ tags.map( item => item.name ).join(" · ") }</p>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
